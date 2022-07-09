97e6648d4b0c934bcb68f1b770eb7d6e418b5699
Bruno Ecq <bruno.ecq.github@gmail.com>
2022-07-09 13:07:31 +0000

    Agregando ejercicio 07

---
+++ 07/01/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <ThisComponente msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import ThisComponente from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ThisComponente
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.title{
+  color:darkslategrey;
+  text-shadow: 1px 2px 2px gray;
+
+}
+</style>
---
+++ 07/01/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="ThisComponente">
+    <span class="title">Primer Componente</span>
+    <span class="title">$el,$props,$data,$options,$refs,$attrs</span>
+    <h1>{{ msg }}</h1>
+    <h2>{{ this.root }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ThisComponente',
+  props: {
+    msg: String,
+  },
+  opcionPersonalizada1:{
+    key: 'value'
+  },
+  opcionPersonalizada2(){
+    alert('soy algo nuevo')
+  },
+  data(){
+    return {
+      root: null
+    }
+  },
+  mounted(){
+    this.root = this.$el
+    console.info(`El elemento raíz de este template, es: ${this.$el}`)
+  }
+}
+</script>
+
+<style scoped>
+.ThisComponente{
+  box-shadow: 2px 5px 5px gray;
+  border: 1px solid gray;
+}
+</style>
